9500bf63d96ee87f70da300a239ab52f1231689d
Ariel Bao <rbao4@jhu.edu>
2020-07-08 14:50:13 -0400

    updated registration

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 import React from "react";
 import SignUp from "./components/SignUp";
 import StartPoll from "./components/StartPoll";
-import PersonalInfo from "./components/Registration/PersonalInfo"
+import PersonalInfo from "./components/Registration/PersonalInfo";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import CreateAccount from './components/Registration/CreateAccount'
-import ShareLocation from './components/Registration/ShareLocation'
-import PersonalInterest from './components/Registration/PersonalInterest'
-import Poll from './components/Poll/Poll'
+import CreateAccount from "./components/Registration/CreateAccount";
+import ShareLocation from "./components/Registration/ShareLocation";
+import PersonalInterest from "./components/Registration/PersonalInterest";
+import Poll from "./components/Poll/Poll";
 
 function App() {
   return (
     <Router>
-    <div>
-      <Switch>
-        <Route path="/" exact component={SignUp} />
-        <Route path="/StartPoll" component={StartPoll} />
-        <Route path='/Registration-personal-info' component={PersonalInfo} />
-        <Route path='/Registration-create-account' component={CreateAccount} />
-        <Route path='/Registration-share-location' component={ShareLocation} />
-        <Route path='/Registration-personal-interest' component={PersonalInterest}/>
-        <Route path='/Poll' component={Poll}/>
-      </Switch>
-    </div>
+      <div>
+        <Switch>
+          <Route path='/' exact component={SignUp} />
+          <Route path='/StartPoll' component={StartPoll} />
+          <Route path='/Registration-personal-info' component={PersonalInfo} />
+          <Route
+            path='/Registration-create-account'
+            component={CreateAccount}
+          />
+          <Route
+            path='/Registration-share-location'
+            component={ShareLocation}
+          />
+          <Route
+            path='/Registration-personal-interest'
+            component={PersonalInterest}
+          />
+          <Route path='/Poll' component={Poll} />
+        </Switch>
+      </div>
     </Router>
   );
 }
