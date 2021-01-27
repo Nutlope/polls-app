9c1de84b6a8377012ad6cf814a0973a94d8c9a3e
Hassan El Mghari <he55@drexel.edu>
2021-01-26 23:09:35 -0500

    deleted firebase and did some clean up

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-//import Firebase, { FirebaseContext } from "./components/Firebase";
-{
-  /* <FirebaseContext.Provider value={new Firebase()}> */
-}
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
